cb550f4aacf6b514096fc887543eb9ec3e148d48
tp323 <tiagopardl@hotmail.com>
2021-05-10 01:05:02 +0100

    Defined Tetriminos
    Defined some of the tetriminos in arrays
    Some are still missing
    Implementation incomplete

--- src/tetris/GameArea.java
+++ src/tetris/GameArea.java
@@ -8,8 +8,53 @@ public class GameArea extends JPanel {
     private final int cellSize = 25;
     private final int cellInterior = 24;
 
-    public GameArea() {
+    //  TETROMINOS
+    /*
+    1   I-block cyan
+        0000
+        ####
+        0000
+        0000
 
+    2   J-block blue
+        0000
+        #000
+        ###0
+        0000
+
+    3   L-block orange
+          #
+        ###
+
+    4   Q-block yellow
+        ##
+        ##
+
+    5   S-block green
+         ##
+        ##
+
+    6   T-block purple
+         #
+        ###
+
+    7   Z-block red
+        ##
+         ##
+    */
+
+    private boolean[][] iblock = new boolean[4][4];
+    private boolean[][] jblock = new boolean[4][4];
+    private boolean[][] lblock = new boolean[4][4];
+    private boolean[][] oblock = new boolean[4][4];
+    private boolean[][] sblock = new boolean[4][4];
+    private boolean[][] tblock = new boolean[4][4];
+    private boolean[][] zblock = new boolean[4][4];
+
+
+
+    public GameArea() {
+        initTetrominos();
         this.setBackground(Color.red);
         this.setVisible(true);
     }
@@ -34,7 +79,56 @@ public class GameArea extends JPanel {
         g.fillRect(cellSize+1,cellSize+1,cellInterior,cellInterior);
         g.fillRect(cellSize*2+1,cellSize+1,cellInterior,cellInterior);
 
+    }
 
+    private void initTetrominos(){
+        clearMatrix(iblock);
+        clearMatrix(jblock);
+        clearMatrix(lblock);
+        clearMatrix(oblock);
+        clearMatrix(sblock);
+        clearMatrix(tblock);
+        clearMatrix(zblock);
+
+        placeBlocks();
+
+
+        for (boolean[] booleans : oblock) {
+            for (int n = 0; n < lblock.length; n++) {
+                System.out.print(booleans[n] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private boolean[][] clearMatrix(boolean[][] block){
+        for(int x=0; x<block.length;x++){
+            for(int y=0; y<block.length;y++){
+                block[x][y] = false;
+            }
+        }return block;
+    }
+
+    private void placeBlocks(){
+        iblock[1][0] = true;
+        iblock[1][1] = true;
+        iblock[1][2] = true;
+        iblock[1][3] = true;
+
+        jblock[1][0] = true;
+        jblock[2][0] = true;
+        jblock[2][1] = true;
+        jblock[2][2] = true;
+
+        lblock[1][2] = true;
+        lblock[2][0] = true;
+        lblock[2][1] = true;
+        lblock[2][2] = true;
+
+        oblock[1][1] = true;
+        oblock[1][2] = true;
+        oblock[2][1] = true;
+        oblock[2][2] = true;
     }
 
 
