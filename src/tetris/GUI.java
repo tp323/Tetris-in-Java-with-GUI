3400f9f5c45b53a085a83aaff1c84e271a933928
tp323 <tiagopardl@hotmail.com>
2021-09-11 20:31:24 +0100

    Tetriminos defined

--- src/tetris/GUI.java
+++ src/tetris/GUI.java
@@ -7,15 +7,13 @@ import java.awt.event.ActionListener;
 
 public class GUI extends JFrame implements ActionListener {
     private int count = 0;
-
     private final int WIDTH = 458, HEIGHT = 565;
 
     private final JLabel label = new JLabel("NUM PLAYS = 0");
-
     private GameArea gameArea;
-
     private JPanel panel = new JPanel();
 
+    public static void main(String[] args) { new GUI(); }
 
     public GUI() {
         JFrame frame = new JFrame();
@@ -23,19 +21,13 @@ public class GUI extends JFrame implements ActionListener {
         JButton button = new JButton("PLAY");
         button.addActionListener(this);
 
-        //panel.setBorder(BorderFactory.createEmptyBorder(50, 50, 100, 100));
-
         panel.add(button);
         panel.add(label);
 
         gameArea = new GameArea();
-        //Grid grid = new Grid();
-
 
         frame.add(panel, BorderLayout.EAST);
-        //frame.add(gameArea, BorderLayout.WEST);
         frame.add(gameArea);
-        //frame.add(grid);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("TETRIS");
         frame.pack();
@@ -43,20 +35,10 @@ public class GUI extends JFrame implements ActionListener {
         frame.setSize(WIDTH, HEIGHT);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
-        //Container contentPane = frame.getContentPane();
-        //contentPane.setLayout(new FlowLayout());
     }
 
     public void actionPerformed(ActionEvent e) {
         count++;
         label.setText("NUM PLAYS = " + count);
     }
-
-
-    public static void main(String[] args) {
-        new GUI();
-    }
-
-
 }
--- src/tetris/GameArea.java
+++ src/tetris/GameArea.java
@@ -8,97 +8,96 @@ public class GameArea extends JPanel {
     private final int cellSize = 25;
     private final int cellInterior = 24;
 
-    //  TETROMINOS
-    /*
+    /*  TETROMINOS
     1   I-block cyan
-        0000
         ####
+        0000
         0000
         0000
 
     2   J-block blue
-        0000
         #000
         ###0
         0000
+        0000
 
     3   L-block orange
-          #
-        ###
+        00#0
+        ###0
+        0000
+        0000
 
     4   Q-block yellow
-        ##
-        ##
+        ##00
+        ##00
+        0000
+        0000
 
     5   S-block green
-         ##
-        ##
+        0##0
+        ##00
+        0000
+        0000
 
     6   T-block purple
-         #
-        ###
+        0#00
+        ###0
+        0000
+        0000
 
     7   Z-block red
-        ##
-         ##
-    */
+        ##00
+        0##0
+        0000
+        0000*/
 
     private boolean[][] iblock = new boolean[4][4];
     private boolean[][] jblock = new boolean[4][4];
     private boolean[][] lblock = new boolean[4][4];
-    private boolean[][] oblock = new boolean[4][4];
+    private boolean[][] qblock = new boolean[4][4];
     private boolean[][] sblock = new boolean[4][4];
     private boolean[][] tblock = new boolean[4][4];
     private boolean[][] zblock = new boolean[4][4];
 
+    Graphics n;
 
 
-    public GameArea() {
-        initTetrominos();
-        this.setBackground(Color.red);
-        this.setVisible(true);
-    }
 
     @Override
     protected void paintComponent(Graphics g){
-
         super.paintComponents(g);
         g.setColor(Color.BLACK);
         g.fillRect(0,0,getWidth(),getHeight());
         g.setColor(Color.WHITE);
-
+        n = g;
         //print grid 20 Lines x 10 Cols
-        for (int x = 0; x <= 250; x += 25)
-            for (int y = 0; y <= 500; y += 25)
-                g.drawRect( x, y, 25, 25);
-
+        for (int x = 0; x <= 250; x += 25) for (int y = 0; y <= 500; y += 25) g.drawRect( x, y, 25, 25);
         g.setColor(Color.RED);
-        //g.drawRect(1,1,25,25);
-        g.fillRect(1,1,cellInterior,cellInterior);
-        g.fillRect(cellSize+1,1,cellInterior,cellInterior);
-        g.fillRect(cellSize+1,cellSize+1,cellInterior,cellInterior);
-        g.fillRect(cellSize*2+1,cellSize+1,cellInterior,cellInterior);
+        printTetromino(g,zblock,1,1);
 
     }
 
+    public GameArea() {
+        initTetrominos();
+    }
+
+    private void printTetromino(Graphics g,boolean[][] block, int x, int y){
+        for(int c = 0; c<4; c++)
+            for(int l = 0; l<4; l++)
+                if(block[c][l]) g.fillRect(cellSize*c + x,cellSize*l + y,cellInterior,cellInterior);
+    }
+
     private void initTetrominos(){
-        clearMatrix(iblock);
+        /*clearMatrix(iblock);
         clearMatrix(jblock);
         clearMatrix(lblock);
-        clearMatrix(oblock);
+        clearMatrix(qblock);
         clearMatrix(sblock);
         clearMatrix(tblock);
-        clearMatrix(zblock);
+        clearMatrix(zblock);*/
 
         placeBlocks();
 
-
-        for (boolean[] booleans : oblock) {
-            for (int n = 0; n < lblock.length; n++) {
-                System.out.print(booleans[n] + " ");
-            }
-            System.out.println();
-        }
     }
 
     private boolean[][] clearMatrix(boolean[][] block){
@@ -110,26 +109,40 @@ public class GameArea extends JPanel {
     }
 
     private void placeBlocks(){
+        iblock[0][0] = true;
         iblock[1][0] = true;
-        iblock[1][1] = true;
-        iblock[1][2] = true;
-        iblock[1][3] = true;
+        iblock[2][0] = true;
+        iblock[3][0] = true;
 
-        jblock[1][0] = true;
-        jblock[2][0] = true;
+        jblock[0][0] = true;
+        jblock[0][1] = true;
+        jblock[1][1] = true;
         jblock[2][1] = true;
-        jblock[2][2] = true;
 
-        lblock[1][2] = true;
         lblock[2][0] = true;
+        lblock[0][1] = true;
+        lblock[1][1] = true;
         lblock[2][1] = true;
-        lblock[2][2] = true;
 
-        oblock[1][1] = true;
-        oblock[1][2] = true;
-        oblock[2][1] = true;
-        oblock[2][2] = true;
+        qblock[0][0] = true;
+        qblock[1][0] = true;
+        qblock[0][1] = true;
+        qblock[1][1] = true;
+
+        sblock[1][0] = true;
+        sblock[2][0] = true;
+        sblock[0][1] = true;
+        sblock[1][1] = true;
+
+        tblock[1][0] = true;
+        tblock[0][1] = true;
+        tblock[1][1] = true;
+        tblock[2][1] = true;
+
+        zblock[0][0] = true;
+        zblock[1][0] = true;
+        zblock[1][1] = true;
+        zblock[2][1] = true;
     }
-
 
 }
